085e6d74052df9b355270deb416a39e4bce67871
ryanl13214 <43986756+ryanl13214@users.noreply.github.com>
2021-05-09 00:21:58 +0100

    Initial commit

---
+++ alpha-capture_the_flag/main.mjs
@@ -0,0 +1,482 @@
+// Note that there is no global objects like Game or Memory. All methods, prototypes and constants are imported built-in modules
+import{	 getObjectsByPrototype,	 getTime,	 RANGED_ATTACK,	 HEAL,	 ATTACK,	 getDirection,	 getDistance,	 getTerrainAt,	 TERRAIN_WALL,	 TERRAIN_SWAMP}from '/game';
+// Everything can be imported either from the root /game module or corresponding submodules
+import{	 Creep,	 StructureTower}from '/game/prototypes';
+import * as PathFinder from '/game/path-finder'; //--> //PathFinder.searchPath
+import{	 Flag,	 BodyPart}from '/arena';
+// You can also import your files like this:
+// import {roleAttacker} from './roles/attacker.mjs';
+// We can define global objects that will be valid for the entire match.
+// The game guarantees there will be no global reset during the match.
+// Note that you cannot assign any game objects here, since they are populated on the first tick, not when the script is initialized.
+let myCreeps, enemyCreeps, enemyFlag, myTower, myFlag,enTower;
+var HeroAttacker = [];
+var HeroRanger = [];
+var Attackers = [];
+var rangers = [];
+var healers = [];
+var mytower = [];
+var safecostmatrix = new PathFinder.CostMatrix;
+const FinalFight = 1600;
+// This is the only exported function from the main module. It is called every tick.
+export function loop()
+{
+	 myCreeps = getObjectsByPrototype(Creep).filter(i => i.my);
+	 enemyCreeps = getObjectsByPrototype(Creep).filter(i => !i.my);
+	 enemyFlag = getObjectsByPrototype(Flag).find(i => !i.my);
+	 myFlag = getObjectsByPrototype(Flag).find(i => i.my);
+	 enTower = getObjectsByPrototype(StructureTower).find(i => !i.my);
+
+	 if(getTime() === 1)
+	 {
+			 for(var x = 0; x < 100; x++)
+			 {
+					 for(var y = 0; y < 100; y++)
+					 {
+							 if(getTerrainAt(x, y) == TERRAIN_WALL)
+							 {
+									 safecostmatrix.set(x, y, 255);
+							 }
+							 else if(getTerrainAt(x, y) == TERRAIN_SWAMP)
+							 {
+									 safecostmatrix.set(x, y, 5);
+							 }
+							 safecostmatrix.set((enTower.x - 50) + x, (enTower.y - 50) + y, 50);
+					 }
+			 }
+			 myCreeps.forEach(creep =>
+			 {
+					 if(creep.body.some(i => i.type === ATTACK)) // creep contains attack part
+					 {
+							 Attackers.push(creep);
+							 creep.healer = 1;
+					 }
+					 if(creep.body.some(i => i.type === RANGED_ATTACK)) // creep contains  ranged attack part
+					 {
+							 if(HeroRanger.length == 0)
+							 {
+									 HeroRanger.push(creep);
+							 }
+							 else if(HeroRanger.length == 1)
+							 {
+									 rangers.push(creep);
+									 creep.healer = 1;
+							 }
+					 }
+			 });
+			 myCreeps.forEach(creep =>
+			 {
+					 if(creep.body.some(i => i.type === HEAL)) // creep contains heal part
+					 {
+							 if(healers.length == 0)
+							 {
+									 creep.targetfollow = Attackers[0];
+									 Attackers[0].healer = creep;
+									 creep.allowGather = true;
+							 }
+							 if(healers.length == 1)
+							 {
+									 creep.targetfollow = Attackers[1];
+									 Attackers[1].healer = creep;
+									 creep.allowGather = true;
+							 }
+							 ////////////////////////////////////////////////////////
+							 if(healers.length == 2)
+							 {
+									 creep.targetfollow = rangers[0];
+									 rangers[0].healer = creep;
+									 creep.allowGather = false;
+							 }
+							 if(healers.length == 3)
+							 {
+									 creep.targetfollow = rangers[1];
+									 rangers[1].healer = creep;
+									 creep.allowGather = false;
+							 }
+							 if(healers.length == 4)
+							 {
+									 creep.targetfollow = rangers[2];
+									 rangers[2].healer = creep;
+									 creep.allowGather = false;
+							 }
+							 if(healers.length == 5)
+							 {
+									 creep.targetfollow = rangers[3];
+									 rangers[3].healer = creep;
+									 creep.allowGather = false;
+							 }
+							 healers.push(creep);
+					 }
+			 });
+	 }
+	 if(getTime() > 20)
+	 {
+			 rangedDefender(HeroRanger[0]);
+			 basicAttacker(Attackers[0]);
+			 basicAttacker(Attackers[1]);
+			 for(var i = 0; i < rangers.length; i++)
+			 {
+					 rangedAttacker(rangers[i]);
+			 }
+			 for(var i = 0; i < healers.length; i++)
+			 {
+					 healer(healers[i]);
+			 }
+			 ///////////////
+			 myTower = getObjectsByPrototype(StructureTower).find(i => i.my);
+			 var range = myTower.store.energy;
+			 var enemiesInRange = enemyCreeps.filter(i => getDistance(i, myTower) < range);
+			 if(enemiesInRange.length > 0)
+			 {
+					 myTower.attack(enemiesInRange[0]);
+			 }
+			 //////////
+	 	 checkForAssaults();
+			 ///////////
+	 }
+}
+
+function moveAllToRallypoint()
+{
+	 if(enemyFlag.x == 2)
+	 {
+			 var rallyppoint = {
+					 "x": 80,
+					 "y": 80
+			 };
+	 }
+	 if(enemyFlag.x == 97)
+	 {
+			 var rallyppoint = {
+					 "x": 20,
+					 "y": 20
+			 };
+	 }
+	 for(var i = 0; i < Attackers.length; i++)
+	 {
+			 Attackers[i].moveTo(rallyppoint);
+	 }
+	 for(var i = 0; i < rangers.length; i++)
+	 {
+			 rangers[i].moveTo(rallyppoint);
+	 }
+	 for(var i = 0; i < healers.length; i++)
+	 {
+			 healers[i].moveTo(rallyppoint);
+	 }
+}
+
+function checkForAssaults()
+{
+	 var enemyCreeps = getObjectsByPrototype(Creep).filter(i => !i.my);
+	 var enemiesInRange = enemyCreeps.filter(i => getDistance(i, enemyFlag) <= 7);
+	 if((enemiesInRange.length < 10 && getTime() > 1000) && enemyCreeps.length > 4)
+	 {
+			 var averageDistance = 0;
+			 var count = 0;
+			 for(var i = 0; i < rangers.length; i++)
+			 {
+					 for(var j = 0; j < rangers.length; j++)
+					 {
+							 averageDistance += getDistance(rangers[i], rangers[j]);
+							 count++;
+					 }
+					 for(var j = 0; j < Attackers.length; j++)
+					 {
+							 averageDistance += getDistance(rangers[i], Attackers[j]);
+							 count++;
+					 }
+			 }
+			 for(var i = 0; i < Attackers.length; i++)
+			 {
+					 for(var j = 0; j < rangers.length; j++)
+					 {
+							 averageDistance += getDistance(Attackers[i], rangers[j]);
+							 count++;
+					 }
+					 for(var j = 0; j < Attackers.length; j++)
+					 {
+							 averageDistance += getDistance(Attackers[i], Attackers[j]);
+							 count++;
+					 }
+			 }
+			 averageDistance = averageDistance / count;
+			 console.log(averageDistance);
+			 var enemiesInRange = enemyCreeps.filter(i => getDistance(i, myFlag) <= 35);
+			 if(averageDistance > 8 && enemiesInRange.length == 0)
+			 {
+					 moveAllToRallypoint();
+			 }
+	 }
+}
+
+function checkForStaticDefence(creep)
+{
+	 var enemyCreeps = getObjectsByPrototype(Creep).filter(i => !i.my);
+	 var enemiesInRange = enemyCreeps.filter(i => getDistance(i, enemyFlag) <= 10);
+	 if((enemiesInRange.length > 10 && getTime() > 35) /* creeps hovering near their own base */ )
+	 {
+			 return true; // collect bodyparts
+	 }
+	 else if((enemyCreeps.length < 3 && getTime() < FinalFight) /*enemy has few creeps left */ )
+	 {
+			 return true; // collect bodyparts
+	 }
+	 else
+	 {
+			 return false; // fight
+	 }
+}
+
+function FindParts(creep, part)
+{
+	 //	console.log("find parts");
+	 var enTower = getObjectsByPrototype(StructureTower).find(i => !i.my);
+	 var bodyparts = getObjectsByPrototype(BodyPart).filter(i => (i.type == part || i.type == "move")).filter(i => getDistance(i, enTower) > 50).sort(i => getDistance(i, creep));
+	 var range = 1;
+	 if(part == "ranged_attack")
+	 {
+			 range = 3;
+	 }
+	 if(part == "heal")
+	 {
+			 var healTargets = myCreeps.filter(i => getDistance(i, creep) <= 3).filter(i => i.hits != i.hitsMax).sort((a, b) => a.hits - b.hits);
+			 if(healTargets.length > 0)
+			 {
+					 if(getDistance(healTargets[0], creep) === 1)
+					 {
+							 creep.heal(healTargets[0]);
+					 }
+					 else
+					 {
+							 creep.rangedHeal(healTargets[0]);
+					 }
+			 }
+	 }
+	 var enemiesInRange = enemyCreeps.filter(i => getDistance(i, creep) <= range);
+	 if(part == "ranged_attack")
+	 {
+			 creep.rangedAttack(enemiesInRange[0]);
+	 }
+	 if(bodyparts.length > 0)
+	 {
+			 Safemove(creep, bodyparts[0]);
+	 }
+}
+
+function basicAttacker(creep)
+{
+	 if(!checkForStaticDefence(creep))
+	 {
+			 var enemyCreeps = getObjectsByPrototype(Creep).filter(i => !i.my);
+			 var targets = enemyCreeps.filter(i => true).sort((a, b) => getDistance(a, creep) - getDistance(b, creep));
+			 var range = 1;
+			 var enemiesInRange = enemyCreeps.filter(i => getDistance(i, creep) <= range);
+			 if(enemiesInRange.length > 0)
+			 {
+					 creep.attack(enemiesInRange[0]);
+					 creep.moveTo(enemiesInRange[0]);
+			 }
+			 else
+			 {
+					 creep.moveTo(targets[0]);
+			 }
+			 slaveHealer(creep, creep.healer);
+	 }
+	 else
+	 {
+			 FindParts(creep, "attack");
+	 }
+}
+
+function slaveHealer(creep, healer)
+{
+	 if(creep.healer != 1)
+	 { // and healer is alive
+			 try
+			 {
+					 var q = getDistance(healer, creep);
+					 if(q > 1)
+					 {
+							 //	console.log("move to healer");
+							 creep.moveTo(creep);
+					 }
+			 }
+			 catch (e)
+			 {}
+	 }
+}
+
+function rangedAttacker(creep)
+{
+	 if(!checkForStaticDefence(creep))
+	 {
+			 var targets = enemyCreeps.filter(i => true).sort((a, b) => getDistance(a, creep) - getDistance(b, creep));
+			 if(targets.length > 0)
+			 {
+					 creep.rangedAttack(targets[0]);
+					 if(getTime() < FinalFight)
+					 {
+							 creep.moveTo(targets[0]);
+					 }
+					 else
+					 {
+							 creep.moveTo(enemyFlag);
+					 }
+			 }
+			 else
+			 {
+					 creep.moveTo(enemyFlag);
+			 }
+			 var range = 3;
+			 var enemiesInRange = enemyCreeps.filter(i => getDistance(i, creep) <= range);
+			 if(enemiesInRange.length > 0)
+			 {
+					 flee(creep, enemiesInRange, range);
+			 }
+	 }
+	 else
+	 {
+			 FindParts(creep, "ranged_attack");
+	 }
+	 if(creep.healer != 1)
+	 {
+			 slaveHealer(creep, creep.healer);
+	 }
+}
+
+function rangedDefender(creep)
+{
+	 var range = 3;
+	 var enemiesInRange = enemyCreeps.filter(i => getDistance(i, creep) <= range);
+	 if(enemiesInRange.length > 0 && enemiesInRange.length < 3)
+	 {
+			 creep.rangedAttack(enemiesInRange[0]);
+	 }
+	 else
+	 {
+			 creep.rangedMassAttack();
+	 }
+	 if(getTime() < FinalFight)
+	 {
+			 creep.moveTo(myFlag);
+	 }
+	 else if(enemyCreeps.length < 3)
+	 {
+			 creep.moveTo(enemyFlag);
+	 }
+}
+
+function moveToHeal(creep)
+{
+	 var myCreeps = getObjectsByPrototype(Creep).filter(i => i.my);
+	 var healTargets = myCreeps.filter(i => i.hits != i.hitsMax).sort((a, b) => a.hits - b.hits);
+	 if(healTargets.length > 0)
+	 {
+			 if(getDistance(healTargets[0], creep) === 1)
+			 {
+					 creep.heal(healTargets[0]);
+			 }
+			 else if(getDistance(healTargets[0], creep) < 4)
+			 {
+					 creep.rangedHeal(healTargets[0]);
+			 }
+			 else
+			 {
+					 creep.moveTo(healTargets[0]);
+			 }
+	 }
+}
+
+function healer(creep)
+{
+	 //console.log("H-" ,JSON.stringify(creep.targetfollow));
+	 if(!checkForStaticDefence(creep))
+	 {
+			 var distcalc = getDistance(enemyFlag, creep)
+			 if(getTime() < 1950 - distcalc && creep.targetfollow.x != undefined)
+			 {
+					 creep.moveTo(creep.targetfollow);
+					 	 creep.heal(creep.targetfollow);
+			 }
+			 else if(getTime() < 1950 - distcalc && creep.targetfollow.x == undefined)
+			 {
+					 moveToHeal(creep);
+			 }
+			 else
+			 {
+					 creep.moveTo(enemyFlag);
+			 }
+
+
+			 var healTargets = myCreeps.filter(i => getDistance(i, creep) <= 3).filter(i => i.hits != i.hitsMax).sort((a, b) => a.hits - b.hits);
+			 if(healTargets.length > 0 && creep.hits > (creep.hitsMax - 200))
+			 {
+					 if(getDistance(healTargets[0], creep) === 1)
+					 {
+							 creep.heal(healTargets[0]);
+					 }
+					 else
+					 {
+							 creep.rangedHeal(healTargets[0]);
+					 }
+			 }
+			 else
+			 {
+					 creep.heal(creep);
+			 }
+	 }
+	 else
+	 {
+			 if(creep.allowGather)
+			 {
+					 FindParts(creep, "heal");
+			 }
+			 else
+			 {
+					 moveToHeal(creep);
+			 }
+	 }
+}
+
+function Safemove(creep, target)
+{
+	 if(creep.currpath == undefined || creep.currpath.path.length == 0)
+	 {
+			 creep.currpath = PathFinder.searchPath(creep, target,
+			 {
+					 costMatrix: safecostmatrix
+			 });
+	 }
+	 else
+	 {
+			 if(creep.currpath.path.length > 0)
+			 {
+					 var direction = getDirection(creep.currpath.path[0].x - creep.x, creep.currpath.path[0].y - creep.y);
+					 var tmp = creep.move(direction);
+					 if(tmp == 0)
+					 {
+							 //	creep.currpath.path =
+							 creep.currpath.path.shift();
+					 }
+					 //	creep.moveTo(target);
+			 }
+	 }
+}
+
+function flee(creep, targets, range)
+{
+	 let result = PathFinder.searchPath(creep, targets.map(i => (
+	 {
+			 pos: i,
+			 range
+	 })),
+	 {
+			 flee: true
+	 });
+	 if(result.path.length > 0)
+	 {
+			 let direction = getDirection(result.path[0].x - creep.x, result.path[0].y - creep.y);
+			 creep.move(direction);
+	 }
+}
